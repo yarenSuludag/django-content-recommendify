b2e6fd0b2c8cf39bf3ce712e5f32706f88500f29
Yaren Suludağ <153204710+yarenSuludag@users.noreply.github.com>
2025-05-02 22:27:35 +0300

    Update settings.py

--- random_recommendations/random_recommendations/settings.py
+++ random_recommendations/random_recommendations/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ue4-1s5=pt_@x!&zn&lv=)rr$d)sy01zb7egc*@d3az@aia%ft'
+SECRET_KEY = 'YOUR_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
